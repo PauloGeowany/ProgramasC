8df387639757536cec48d85fee3a723d84bcc52e
Paulo <paulogeodias@academico.ufs.br>
2025-06-03 22:03:16 -0300

    mudando compilador pra 64 bits

--- ponteiros.c
+++ ponteiros.c
@@ -4,8 +4,10 @@
 int main() {
 int var = 15;
 int *ptr = &var;
+float var2 = 15.5;
 printf("O conteudo de var eh: %d\n",var);
 printf("O endereco de var eh: %p\n", &var);
 printf("Conteudo apontado por ptr: %d\n", *ptr);
 printf("Endereco do ponteiro eh: %p\n", &ptr);
+printf("tamanho de ptr eh: %zu\n", sizeof(ptr));
 }
