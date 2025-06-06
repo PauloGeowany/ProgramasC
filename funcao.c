d83721bf517ba4890113bffc6ad746267da6e97c
Paulo <paulogeodias@academico.ufs.br>
2025-06-06 11:33:28 -0300

    ;

--- funcao.c
+++ funcao.c
@@ -1,8 +1,18 @@
-int soma(a,b){
+#include <stdio.h>
+
+int soma(int a,int b){
     int soma = a+b;
     return soma;
 }
 
 int main(){
-    
+    int a,b;
+   printf("Insira o numero a: "); 
+    scanf("%d", &a);
+   printf("Insira o numero b: "); 
+    scanf("%d", &b);
+
+    int resultado = soma(a,b);
+    printf("A soma entre a e b eh %d", resultado);
+
 }
